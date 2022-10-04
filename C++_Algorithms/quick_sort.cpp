2178e714098fe63fd1673b5cef0b05da5f19517e
Prayag Raj Mathuria <prayag07.mathuria@gmail.com>
2022-10-04 22:20:45 +0530

    added quick_sort.cpp in c++_Algorithms folder

---
+++ C++_Algorithms/quick_sort.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+  
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high]; // pivot
+    int i
+        = (low
+           - 1); // Index of smaller element and indicates
+                 // the right position of pivot found so far
+  
+    for (int j = low; j <= high - 1; j++) {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+  
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high) {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+  
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+  
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+  
+// Function to input array elements
+void fill_elements(int arr[], int n){
+    cout << "Enter the array elements\n";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of the array.\n";
+    cin >> n;
+    int arr[n];
+    fill_elements(arr,n);
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+  
